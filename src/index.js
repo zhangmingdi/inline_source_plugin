af6e2d87a4e1eca71d8d984fd471d8520db090b1
zhangmingdi <3201896772@qq.com>
2021-12-12 22:39:02 +0800

    项目初始化

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './index.css'
+
+console.log('kjshkjdshkhskjhsakjhs');
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+    ],
+}
